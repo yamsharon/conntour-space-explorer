ee91d7e336b09c5d53990c2a85c98e35fcb503b5
Yam Sharon <yamsharon102@gmail.com>
2025-12-15 21:58:32 +0200

    Added logs to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,8 +4,15 @@ from app.domain.models import Source
 from app.infra.db import SpaceDB
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.utils.logger import logger
+from dotenv import load_dotenv
+
+load_dotenv(override=True)   # Load environment variables from .env file
+logger.info("Environment variables loaded")
 
 app = FastAPI()
+
+logger.info("Starting the Conntour Space Explorer backend")
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,7 +27,9 @@ db = SpaceDB()
 
 @app.get("/api/sources", response_model=List[Source])
 def get_sources():
+    logger.info("Getting all sources")
     sources = db.get_all_sources()
+    logger.info(f"Found {len(sources)} sources")
     return sources
 
 
