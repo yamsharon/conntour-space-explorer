efd276e02b4f383b7603d7486798d162141b8825
Yam Sharon <yamsharon102@gmail.com>
2025-12-15 23:29:51 +0200

    Added three required pages

---
+++ frontend/src/pages/SearchPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchPage: React.FC = () => {
+  return (
+    <div className="py-8">
+      <p className="text-gray-700">Search coming soon.</p>
+    </div>
+  );
+};
+
+export default SearchPage;
+
+
