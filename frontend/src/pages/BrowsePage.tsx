b9e9ba383bef0928905cb1f3955d98e9b65e925a
Yam Sharon <yamsharon102@gmail.com>
2025-12-15 23:29:51 +0200

    Added three required pages

---
+++ frontend/src/pages/BrowsePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Sources from '../components/Sources';
+
+const BrowsePage: React.FC = () => {
+  return <Sources />;
+};
+
+export default BrowsePage;
+
+
---
+++ frontend/src/pages/HistoryPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HistoryPage: React.FC = () => {
+  return (
+    <div className="py-8">
+      <p className="text-gray-700">History coming soon.</p>
+    </div>
+  );
+};
+
+export default HistoryPage;
+
+
