ffd443a3625c26feb7e1382c5a2892ee1146a0cc
Yam Sharon <yamsharon102@gmail.com>
2025-12-17 21:06:32 +0200

    Switched lm models

--- backend/app/infra/language_model.py
+++ backend/app/infra/language_model.py
@@ -1,20 +1,19 @@
-from sentence_transformers import SentenceTransformer
+
+from transformers import CLIPProcessor, CLIPModel
 
 from app.utils.logger import logger
 
 
 class LanguageModel:
-    MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+    """Language model for generating embeddings."""
+
+    MODEL_NAME = "openai/clip-vit-base-patch32"
 
     def __init__(self):
+        """Initialize the LanguageModel."""
         logger.info("Initializing a language model with sentence-transformers")
-        try:
-            # Using all-MiniLM-L6-v2: fast, good quality, 384 dimensions
-            # Possible to use a better model, but it may cause performance issues
-            # I chose this lean one as one of the requirement was -
-            #   the browsing experience should be clear, visually friendly...
-            self.model = SentenceTransformer(LanguageModel.MODEL_NAME)
-            logger.info(f"Model loaded: {LanguageModel.MODEL_NAME}")
-        except Exception as e:
-            logger.error(f"Failed to load model: {e}")
-            raise
+        # Load the model and processor
+        self.model = CLIPModel.from_pretrained(LanguageModel.MODEL_NAME)
+        logger.debug(f"Model loaded: {LanguageModel.MODEL_NAME}")
+        self.processor = CLIPProcessor.from_pretrained(LanguageModel.MODEL_NAME)
+        logger.debug(f"Processor loaded: {LanguageModel.MODEL_NAME}")
