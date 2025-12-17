759aa10ffda344e041a60a32181a24e6625dd161
Yam Sharon <yamsharon102@gmail.com>
2025-12-17 21:06:01 +0200

    Added trace info to logger

---
+++ backend/app/utils/constants.py
@@ -0,0 +1,9 @@
+import os.path
+
+# Data Paths
+MOCK_DATA_JSON = os.path.join("data", "mock_data.json")
+EMBEDDING_CACHE = os.path.join("data", "embeddings_cache.pkl")
+DEFAULT_LOG_PATH = os.path.join("logs", "app.log")
+
+# Keys
+EMBEDDING_KEY = "embedding"
